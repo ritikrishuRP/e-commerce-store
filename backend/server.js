d461364c0beaad8594f2ca84ef7238ec88d1b881
rishu <rrishu212@gmail.com>
2025-03-06 18:33:59 +0530

    Auth Refresh & Access Token

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+//routes
+import authRoutes from "./routes/auth.route.js"
+
+import { connectDB } from "./lib/db.js";
+dotenv.config(); //Allows us to read the content of the dot env file
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());  //for parsing application/json
+app.use(express.urlencoded({ extended: true })); //for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+    console.log("Server is running on port http://localhost:" + PORT);
+
+    connectDB();
+})
+
