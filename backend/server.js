122487a6c6fa2011e274df932847050696f4604c
rishu <rrishu212@gmail.com>
2025-03-10 17:01:13 +0530

    coupon route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
 import cartRoutes from "./routes/cart.route.js"
+import couponRoutes from "./routes/coupon.route.js"
 
 import { connectDB } from "./lib/db.js";
 dotenv.config(); //Allows us to read the content of the dot env file
@@ -19,7 +20,8 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
-app.use("/api/carts", cartRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on port http://localhost:" + PORT);
